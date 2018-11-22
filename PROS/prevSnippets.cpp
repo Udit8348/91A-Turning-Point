c4c08f0866cec39e5bb71c34b2bc4d04d6df03b0
Udit Subramanya <udit610@yahoo.com>
2018-11-21 20:17:50 -0500

    Update prevSnippets.cpp

--- PROS/prevSnippets.cpp
+++ PROS/prevSnippets.cpp
@@ -56,3 +56,55 @@ if(Controller1.get_digital(pros::E_CONTROLLER_DIGITAL_L2)){
   		}
 				dscr.set_brake_mode( pros::E_MOTOR_BRAKE_HOLD);
 		}
+
+
+//Testing Flywheel Function, uses built in V5 velocity controller. Too agressive for flywheeel, no longer in use.
+int targetRPM = 0;
+int a = 0;
+int b = 0;
+//std::string zVel = "0";
+//this function controls the flwheel
+void flywheel() {
+	/*
+  if(vexRT[Btn8L] == 1) {targetPow = 64;	targetRPM = 136; I=0; segment=0; } // base
+if(vexRT[Btn8U] == 1) {targetPow = 53;	targetRPM = 118; I=0; segment=0; } // mid-field
+if(vexRT[Btn8R] == 1) {targetPow = 42;	targetRPM = 100; I=0; segment=0; } // at the bar
+if(vexRT[Btn8D] == 1) {targetPow =  0;	targetRPM =   0; I=0; segment=0; }
+
+if(vexRT[Btn6U]==1 && a==0) {targetRPM++; targetPow+=0.5; a=1;}	else if(vexRT[Btn6U]==0) a=0;
+if(vexRT[Btn6D]==1 && b==0) {targetRPM--; targetPow-=0.5; b=1;}	else if(vexRT[Btn6D]==0) b=0;
+
+  */
+if(Controller1.get_digital(pros::E_CONTROLLER_DIGITAL_A)){targetRPM = 0;}
+if(Controller1.get_digital(pros::E_CONTROLLER_DIGITAL_X)){targetRPM = -100;}
+if(Controller1.get_digital(pros::E_CONTROLLER_DIGITAL_Y)){targetRPM = -150;}
+if(Controller1.get_digital(pros::E_CONTROLLER_DIGITAL_B)){targetRPM = -200;}
+
+if(Controller1.get_digital(pros::E_CONTROLLER_DIGITAL_UP) && a == 0){
+  targetRPM+= -1;
+  a=1;
+}else if(Controller1.get_digital(pros::E_CONTROLLER_DIGITAL_UP)){
+  a=0;
+}
+
+if(Controller1.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN) && b == 0){
+  targetRPM++;
+  b=1;
+}else if(Controller1.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)){
+  b=0;
+}
+//print velocity to the brain screen. This code did not work, .set_text does not allow text formatting. Fixed in newest version
+//zVel = std::to_string(targetRPM);//@@ try this
+//pros::lcd::set_text(0, "Flywheel Velocity");
+//pros::lcd::set_text(1, zVel);
+	
+ //printing to controller only works with 100ms delay. this program printed to fast for controller to print correctly. Fixed in new version
+ Controller1.print(0, 0, "RPM: %d", targetRPM);
+
+//set velocity to the motor
+fw.move_velocity(targetRPM);
+
+
+}//end of function
+
+
